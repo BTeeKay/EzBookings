f7f749f89aa5506be278de2f32606312798f39d9
Brian Kerr <briantk91@outlook.com>
2023-12-27 22:10:21 +0000

    added example entity from memory

---
+++ EzBookings/Configuration/DriverConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EzBookings.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EzBookings.Configuration
+{
+    public class DriverConfiguration : IEntityTypeConfiguration<DriverEntity>
+    {
+        public void Configure(EntityTypeBuilder<DriverEntity> builder)
+        {
+            builder.Property(d => d.FirstName)
+                .IsUnicode(true)
+                .HasMaxLength(36)
+                .IsRequired();
+
+            builder.Property(d => d.LastName)
+                .IsUnicode(true)
+                .HasMaxLength(36)
+                .IsRequired();
+        }
+    }
+}
---
+++ EzBookings/Entities/DriverEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace EzBookings.Entities
+{
+    public class DriverEntity
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    }
+}
